a97fa6883ae50762d7139f13b144d831680f4f85
Nhật Dương <duongintech@gmail.com>
2024-02-01 17:43:35 +0700

    update code

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useStore } from '../store/store.tsx'
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from "../theme/theme.ts";
+import HeaderBar from "../components/HeaderBar.tsx";
+import CustomIcon from "../components/CustomIcon.ts";
 
 const getCategoriesFromData = (data: any) => {
   let temp: any = {};
@@ -16,33 +20,97 @@ const getCategoriesFromData = (data: any) => {
   return categories;
 };
 
-// const getCoffeeList = (category: string, data: any) => {
-//   if (category == 'All') {
-//     return data;
-//   } else {
-//     let coffeelist = data.filter((item: any) => item.name == category);
-//     return coffeelist;
-//   }
-// };
+const getCoffeeList = (category: string, data: any) => {
+  if (category == 'All') {
+    return data;
+  } else {
+    let coffeelist = data.filter((item: any) => item.name == category);
+    return coffeelist;
+  }
+};
 
 const HomeScreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList)
   const BeanList = useStore((state: any) => state.BeanList)
-  const [categories, setCategories] = useState([undefined])
-  const [searchText, setSearchText] = useState(undefined)
+  const [categories, setCategories] = useState(getCategoriesFromData(CoffeeList))
+  const [searchText, setSearchText] = useState('')
   const [categoryIndex, setCategoryIndex] = useState({
     index: 0,
     category: categories[0],
   });
-  const [sortedCoffee, setSortedCoffee] = useState(undefined);
+  const [sortedCoffee, setSortedCoffee] = useState(getCoffeeList(categoryIndex.category, CoffeeList));
+  const tabBarHeight = useBottomTabBarHeight();
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+      <ScrollView  showsHorizontalScrollIndicator={false} contentContainerStyle={styles.ScrollViewFlex}>
+      {/*  App Header*/}
+        <HeaderBar />
+        
+        <Text style={styles.ScreenTitle}>
+          Find the best{'\n'}coffee for you
+        </Text>
+        {/* Search Input */}
+
+        <View style={styles.InputContainerComponent}>
+          <TouchableOpacity onPress={() => { }}>
+            <CustomIcon
+              style={styles.InputIcon}
+              name="search"
+              size={FONTSIZE.size_18}
+              color={
+                searchText.length > 0
+                  ? COLORS.primaryOrangeHex
+                  : COLORS.primaryLightGreyHex
+              }/>
+          </TouchableOpacity>
+          <TextInput
+            placeholder="Find Your Coffee..."
+            value={searchText}
+            onChangeText={text => {
+              setSearchText(text);
+            }}
+            placeholderTextColor={COLORS.primaryLightGreyHex}
+            style={styles.TextInputContainer}
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer:{
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex
+  },
+  ScrollViewFlex:{
+    flexGrow: 1,
+  },
+  ScreenTitle: {
+    fontSize: FONTSIZE.size_28,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    color: COLORS.primaryWhiteHex,
+    paddingLeft: SPACING.space_30,
+  },
+  InputContainerComponent: {
+    flexDirection: 'row',
+    margin: SPACING.space_30,
+    borderRadius: BORDERRADIUS.radius_20,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+    alignItems: 'center',
+  },
+  InputIcon: {
+     marginHorizontal: SPACING.space_20,
+  },
+  TextInputContainer: {
+    flex: 1,
+    height: SPACING.space_20 * 3,
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.primaryWhiteHex,
+  },
+})
 
 export default HomeScreen
